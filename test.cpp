2a03633127c31d7c3fff16c6cb09080b8382df06
oussamaMAZ <70782166+oussama-maz2000@users.noreply.github.com>
2022-06-09 12:57:09 +0100

    ...

--- test.cpp
+++ test.cpp
@@ -67,7 +67,7 @@ int main (){
     {
         for (int i = 0; i < 10; i++)
         {
-            cout<<vecOfStr.size()<<endl;
+            cout<<vecOfStr[i]<< endl;
             
         }
         
